1e6aa15087b2ee2000a199ff025a4c74555ddb6f
NineSenz <1003823662@qq.com>
2021-05-19 13:51:01 +0800

    设置页面局部样式

---
+++ pages/message/message.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<h1>Message</h1>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
