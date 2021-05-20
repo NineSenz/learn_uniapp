4245458b783f674769cd7dc7a134a930037ba32f
NineSenz <1003823662@qq.com>
2021-05-20 17:54:13 +0800

    图片基本使用

---
+++ pages/image/image.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<image src="../../static/logo.png">本地图片</image>
+		<image src="http://b.zol-img.com.cn/desk/bizhi/image/1/1680x1050/1349289433496.jpg">网络图片</image>
+		<image src="http://b.zol-img.com.cn/desk/bizhi/image/1/1680x1050/1349289433496.jpg" mode="aspectFit">网络图片</image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
