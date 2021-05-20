66476563ed6839949e509cfa74acc69f15e64097
NineSenz <1003823662@qq.com>
2021-05-20 17:45:15 +0800

    按钮的基本使用

---
+++ pages/button/button.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<button>按钮</button>
+		<button size="mini">小按钮</button>
+		<button type="primary">默认按钮</button>
+		<button type="primary" plain>镂空按钮</button>
+		<button type="primary" disabled>禁用按钮</button>
+		<button loading="">加载中</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
