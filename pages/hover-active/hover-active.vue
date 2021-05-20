a97c307fa2ca3cf0384f27e9dead3aab16f2c1a5
NineSenz <1003823662@qq.com>
2021-05-20 17:36:37 +0800

    view的基本应用，解决事件冒泡

---
+++ pages/hover-active/hover-active.vue
@@ -0,0 +1,44 @@
+<template>
+	<view>
+		<view class="box" hover-class="box-active">
+			<view class="box2" hover-class="box2-active" hover-stop-propagation>小盒子</view>
+		</view>
+		<view>
+			<p>
+				<b>hover-stop-propagation</b>属性可以解决事件冒泡的问题
+			</p>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.box {
+		width: 200px;
+		height: 200px;
+		background: #007AFF;
+	}
+	.box-active {
+		background: red;
+	}
+	.box2 {
+		width: 100px;
+		height: 100px;
+		background: #4CD964;
+	}
+	.box2-active {
+		background: #F0AD4E;
+	}
+</style>
