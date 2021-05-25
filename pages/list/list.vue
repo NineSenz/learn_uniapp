fea319c18b96c33be22e39be4d7a7fea6834c45d
NineSenz <1003823662@qq.com>
2021-05-25 16:40:33 +0800

    下拉刷新

---
+++ pages/list/list.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<view >
+			<ul v-for="item in list">
+				<li>{{item}}</li>
+			</ul>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg: "hello",
+				list: ['frontend','backend', 'database','testcase', 'application']
+			}
+		},
+		onPullDownRefresh(){
+			setTimeout(()=>{
+				this.list = ['testcase', 'application','frontend','backend', 'database']
+				uni.stopPullDownRefresh()
+			},2000)
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
