6a849198c2c8a47419b5025f1b16d7495190a7c5
NineSenz <1003823662@qq.com>
2021-05-25 16:56:34 +0800

    发送get请求

---
+++ pages/request/request.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<button type="primary" size="mini" @click="sendGet">发送请求</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			sendGet(){
+				uni.request({
+					url: 'localhost:8080/home/swiper',
+					success(res){
+						console.log(res)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
